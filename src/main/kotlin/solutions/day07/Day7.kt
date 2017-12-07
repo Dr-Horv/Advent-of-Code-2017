7008a9085f73a9c3ccbad18fabf9e4235421d0ee
Johan Horvius <drhorv@gmail.com>
2017-12-07 08:16:44 +0100

    Cleanup of day7

--- src/main/kotlin/solutions/day07/Day7.kt
+++ src/main/kotlin/solutions/day07/Day7.kt
@@ -1,4 +1,3 @@
-
 package solutions.day07
 
 import solutions.Solver
@@ -9,95 +8,96 @@ import java.util.*
 data class Program(val name: String, val weight: Int, var parent: Program?, var children: List<Program>, var totalWeight: Int, var level: Int)
 data class Tuple(val program: Program, val diff: Int)
 
-class LevelComparator: Comparator<Program> {
+class LevelComparator : Comparator<Program> {
     override fun compare(p0: Program?, p1: Program?): Int {
         return p1!!.level - p0!!.level
     }
 }
 
-class Day7: Solver {
+class Day7 : Solver {
 
 
     override fun solve(input: List<String>, partTwo: Boolean): String {
-        val map: MutableMap<String, Program> = mutableMapOf()
-        val programsWithChildren : MutableMap<Program, List<String>> = mutableMapOf()
-        val parentsMap: MutableMap<String, Program> = mutableMapOf()
-
-        input
-                .map { it.split("->").map { it.trim() } }
-                .forEach {
-                    val firstParts = it[0].splitAtWhitespace()
-                    val name = firstParts[0]
-                    val weightStr = firstParts[1]
-                    val weight = weightStr.substring(1, weightStr.lastIndex).toInt()
-                    val p = Program(name, weight, null, mutableListOf(), weight, -1)
-                    map.put(name, p)
-                    if(it.size > 1) {
-                        val children = it[1].split(",").map(String::trim)
-                        programsWithChildren.put(p, children)
-                        children.forEach { parentsMap.put(it, p) }
-                    }
-                }
-
-        programsWithChildren.forEach {(p,cs) ->
-            val children = cs.map { map.getValue(it) }.toList()
-            p.children = children
-            children.forEach { it.parent = p }
-        }
-
-        val k = map.keys.first()
-
-        val firstProgram = map.getValue(k)
-
-        val root = findRoot(firstProgram)
-
-        if(!partTwo) {
+        val map: Map<String, Program> = constructTree(input)
+        val root = findRoot(map.values.first())
+        if (!partTwo) {
             return root.name
         }
 
         correctLevels(root)
+        correctTotalWeights(map)
 
-        val queue : PriorityQueue<Program> = PriorityQueue(LevelComparator()) //map.values.filter({it.children.isEmpty()}).toCollection(LinkedList())
-
-
-        queue.addAll(map.values)
-
-        var sanity = 0
-        while (queue.isNotEmpty()) {
-            sanity++
-            val next = queue.poll()
-            if(next.parent != null) {
-                val parent = next.parent!!
-                parent.totalWeight += next.totalWeight
-                if(!queue.any { p -> p.name == parent.name }){
-                    queue.add(parent)
-                }
-            }
-        }
-        
         val (p, diff) = findCulprit(root)
 
         return (p.weight + diff).toString()
 
     }
 
-    private fun correctLevels(root: Program, level:Int = 0) {
+    private fun correctTotalWeights(map: Map<String, Program>) {
+        val queue: Queue<Program> = PriorityQueue(LevelComparator())
+        queue.addAll(map.values)
+
+        while (queue.isNotEmpty()) {
+            val next = queue.poll()
+            if (next.parent != null) {
+                val parent = next.parent!!
+                parent.totalWeight += next.totalWeight
+                if (!queue.contains(parent)) {
+                    queue.add(parent)
+                }
+            }
+        }
+    }
+
+    private fun constructTree(input: List<String>): Map<String, Program> {
+        val map: MutableMap<String, Program> = mutableMapOf()
+        val programsWithChildren: MutableMap<Program, List<String>> = mutableMapOf()
+        input
+                .map { it.split("->").map(String::trim) }
+                .forEach {
+                    createProgramNode(it, map, programsWithChildren)
+                }
+
+        programsWithChildren.forEach { (p, cs) ->
+            val children = cs.map { map.getValue(it) }.toList()
+            p.children = children
+            children.forEach { it.parent = p }
+        }
+
+        return map
+    }
+
+    private fun createProgramNode(parts: List<String>, map: MutableMap<String, Program>, programsWithChildren: MutableMap<Program, List<String>>) {
+        val firstParts = parts[0].splitAtWhitespace()
+        val name = firstParts[0]
+        val weightStr = firstParts[1]
+        val weight = weightStr.substring(1, weightStr.lastIndex).toInt()
+        val p = Program(name, weight, null, mutableListOf(), weight, -1)
+        map.put(name, p)
+        if (parts.size > 1) {
+            val children = parts[1].split(",").map(String::trim)
+            programsWithChildren.put(p, children)
+
+        }
+    }
+
+    private fun correctLevels(root: Program, level: Int = 0) {
         root.level = level
-        root.children.forEach { correctLevels(it, level+1) }
+        root.children.forEach { correctLevels(it, level + 1) }
     }
 
     private fun findCulprit(root: Program): Tuple {
         val weightSorted = root.children.sortedBy { it.totalWeight }
 
-        if(weightSorted.size < 3) {
+        if (weightSorted.size < 3) {
             throw RuntimeException("Cannot determine too heavy or too light")
         }
 
         val first = weightSorted.first()
         val last = weightSorted.last()
-        val middle = weightSorted[weightSorted.size/2]
+        val middle = weightSorted[weightSorted.size / 2]
 
-        return if(first.totalWeight != middle.totalWeight) {
+        return if (first.totalWeight != middle.totalWeight) {
             findLighter(first, first.totalWeight - middle.totalWeight)
         } else {
             findHeavier(last, middle.totalWeight - last.totalWeight)
@@ -108,7 +108,7 @@ class Day7: Solver {
 
     private fun findHeavier(p: Program, diff: Int): Tuple {
         val weightSorted = p.children.sortedBy { it.totalWeight }
-        return if(weightSorted.first().totalWeight == weightSorted.last().totalWeight) {
+        return if (weightSorted.first().totalWeight == weightSorted.last().totalWeight) {
             Tuple(p, diff)
         } else {
             findHeavier(weightSorted.last(), diff)
@@ -117,27 +117,18 @@ class Day7: Solver {
 
     private fun findLighter(p: Program, diff: Int): Tuple {
         val weightSorted = p.children.sortedBy { it.totalWeight }
-        return if(weightSorted.first().totalWeight == weightSorted.last().totalWeight) {
+        return if (weightSorted.first().totalWeight == weightSorted.last().totalWeight) {
             Tuple(p, diff)
         } else {
             findLighter(weightSorted.first(), diff)
         }
     }
 
-    private fun addWeightUpwards(p: Program, w: Int) {
-        p.totalWeight += w
-        if(p.parent != null) {
-            addWeightUpwards(p.parent!!, w)
-        }
-
-    }
-
     private fun findRoot(program: Program): Program {
-        var curr = program
-        return if(curr.parent != null) {
-            findRoot(curr.parent!!)
+        return if (program.parent != null) {
+            findRoot(program.parent!!)
         } else {
-            curr
+            program
         }
     }
 }
